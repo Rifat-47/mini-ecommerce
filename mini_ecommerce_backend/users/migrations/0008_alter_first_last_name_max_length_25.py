0fba6c873280025ab9e3804c1694c3f3c538cdfe
rifat <rifatscc1747@gmail.com>
2026-05-10 17:03:29 +0600

    deployment fail issue 2

--- mini_ecommerce_backend/users/migrations/0008_alter_first_last_name_max_length_25.py
+++ mini_ecommerce_backend/users/migrations/0008_alter_first_last_name_max_length_25.py
@@ -13,11 +13,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='first_name',
-            field=models.CharField(blank=True, max_length=25),
+            field=models.CharField(blank=True, max_length=150),
         ),
         migrations.AlterField(
             model_name='user',
             name='last_name',
-            field=models.CharField(blank=True, max_length=25),
+            field=models.CharField(blank=True, max_length=150),
         ),
     ]
